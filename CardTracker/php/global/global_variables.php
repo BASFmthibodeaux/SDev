a7f6aaa93a5e1e2e4c82ef861f3511505e3e0dc0
Javier <javier.loucim@gmail.com>
2011-12-13 22:29:37 -0300

    no message

--- CardTracker/php/global/global_variables.php
+++ CardTracker/php/global/global_variables.php
@@ -12,19 +12,9 @@
 */
 	$functions_path="../global";
 	$main_database = "sdevcoma_moneytrack";
-    $base_seguridad = "m";
-    $base_contenidos = "rad";
-	$Home = "<b><a href='../main/show_home.php'><font color='#000000'>Home</font></a></b>";
-    $Separador = "<font color='#3399ff'>&nbsp;/&nbsp;</font>";
     $separador_decimal = ".";
-    $separador_miles = ",";
-    $mensaje_session_expired = "../main/show_login.php";
-    $directorio_imagenes_subcategorias = "images/subcategorias/";
-    $directorio_imagenes_categorias = "images/categorias/";
-    $directorio_imagenes_productos = "images/productos/";
     
 //    $php_command_line = "/usr/local/bin/php";
-    $php_command_line = "c:\sistemas\software\php\php.exe";
 
     $admin = 1;
     //TODO: Esto tiene que reemplazarse buscando una cookie en el cliente

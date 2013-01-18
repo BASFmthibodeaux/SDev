8264a357db97baa5e5d953b3258232221c8c25e8
Javier <javier.loucim@gmail.com>
2013-01-18 08:41:20 -0300

    cambio de directorios del Kendo

---
+++ iOS/XMLTest/XMLTest/XMLTest/FirstViewController.m
@@ -0,0 +1,29 @@
+//
+//  FirstViewController.m
+//  XMLTest
+//
+//  Created by Javi on 17/01/13.
+//  Copyright (c) 2013 Javi. All rights reserved.
+//
+
+#import "FirstViewController.h"
+
+@interface FirstViewController ()
+
+@end
+
+@implementation FirstViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ iOS/XMLTest/XMLTest/XMLTest/SecondViewController.m
@@ -0,0 +1,29 @@
+//
+//  SecondViewController.m
+//  XMLTest
+//
+//  Created by Javi on 17/01/13.
+//  Copyright (c) 2013 Javi. All rights reserved.
+//
+
+#import "SecondViewController.h"
+
+@interface SecondViewController ()
+
+@end
+
+@implementation SecondViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
